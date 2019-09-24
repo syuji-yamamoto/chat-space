2522c1f05f471dc86a81db8a43392cd55b561001
yamamoto syuji <aitaiforty@yahoo.co.jp>
2019-09-24 12:52:17 +0900

    ユーザー名修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ lock '3.11.1'
 
 set :application, 'chat-space'
 
-set :repo_url,  'git@github.com:syuji_yamamoto/chat-space.git'
+set :repo_url,  'git@github.com:syuji-yamamoto/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
